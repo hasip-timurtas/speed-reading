f988dc7d455a9af5eb0471ddac16c701f8a4ce6d
hasip <hasip.timurtas@gmail.com>
2016-05-20 20:39:11 +0300

    homepage designed

--- client/App/App.jsx
+++ client/App/App.jsx
@@ -4,49 +4,125 @@ export default class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            theText: ''
+            theText: "Bu uygulama ile okuma hızınız inanılmaz bir şekilde artacak. İlk başlarda çok hızlı olduğunu" +
+            " düşüneceksiniz fakar ilerleyen zamanlarda bu durumun ne kadar zevkli olduğunu anlayacak ve okuma" +
+            " hızınızı inanılmaz bir şekilde geliştireceksiniz. her bölümün sonunda okuma hızınızı bir tık" +
+            " artıracaksınız",
+            word: '',
+            readingSpeed: 250,
+            timer: null
         }
     }
 
+    decreaseSpeed() {
+        this.stopTimer();
+        var currentSpeed = this.state.readingSpeed;
+        this.setState({readingSpeed: currentSpeed - 50});
+    }
+
+    increaseSpeed() {
+        this.stopTimer();
+        var currentSpeed = this.state.readingSpeed;
+        this.setState({readingSpeed: currentSpeed + 50});
+    }
+
     componentDidMount() {
-        this.getExample();
+        //this.getExample();
+    }
+
+    changeText() {
+        var text = this.refs.theText.value;
+        this.setState({theText: text});
     }
 
     render() {
         return (
             <div className="row">
-                <div className="col-md-8">
-                    <div className="reading-area">
-                        {this.state.theText}
+                <div className="col-md-8 reading-area">
+                    <div className="control-area input-group">
+                        <input type="text"
+                               className="form-control text-speed"
+                               value={this.state.readingSpeed}
+                               readOnly={true}/>
+
+                        <input type="button" value="-" className="btn btn-default"
+                               onClick={this.decreaseSpeed.bind(this)}/>
+
+                        <input type="button" value="+" className="btn btn-default"
+                               onClick={this.increaseSpeed.bind(this)}/>
+                        <br/>
+                        <input type="button" value="Start" className="btn btn-default"
+                               onClick={this.getExample.bind(this)}/>
+
+                        <input type="button" value="Stop" className="btn btn-default"
+                               onClick={this.stopTimer.bind(this)}/>
                     </div>
+                    <br/>
+                    <div className="reading-text">
+                        <span>{this.state.word}</span>
+                    </div>
+                    <br/>
+                    <textarea cols="30" rows="10"
+                              className="form-control"
+                              value={this.state.theText}
+                              ref="theText"
+                              onChange={this.changeText.bind(this)}
+                    />
+
                 </div>
             </div>
         )
     }
 
     getExample() {
-        var text = "Bu uygulama ile okuma hızınız inanılmaz bir şekilde artacak. İlk başlarda çok hızlı olduğunu" +
-            " düşüneceksiniz fakar ilerleyen zamanlarda bu durumun ne kadar zevkli olduğunu anlayacak ve okuma" +
-            " hızınızı inanılmaz bir şekilde geliştireceksiniz. her bölümün sonunda okuma hızınızı bir tık" +
-            " artıracaksınız";
+        var text = this.state.theText;
 
-        var interval = 250;
+        var readingSpeed = this.state.readingSpeed;
+        var interval = 1000 / (readingSpeed / 60);
+        console.log(interval);
         var index = 0;
 
         var self = this;
 
-        var timer = setInterval(function () {
-            self.changeText(text.split(" ")[index]);
+        this.state.timer = setInterval(function () {
+            self.changeWord(text.split(" ")[index]);
+
+            console.log(text.split(" ")[index]);
+
             index++;
+
+            if (index + 1 > text.split(" ").length) {
+                clearTimeout(self.state.timer);
+            }
+
         }, interval);
 
-        if(index > text.split(" ").length)
-        {
-            clearTimeout(timer);
-        }
+
     }
 
-    changeText(text){
-        this.setState({theText: text});
+    stopTimer() {
+        clearTimeout(this.state.timer);
+    }
+
+    changeWord(text) {
+        this.setState({word: this.makecolorful(text)});
+    }
+
+    makecolorful(text) {
+        return text;
+
+        var textLenght = Math.floor(text.length / 2);
+        //console.log(textLenght);
+        var newText = "";
+
+        for (i = 0; i < text.length; i++) {
+            if (i == textLenght) {
+                newText += [<span className='red-letter'>  text[i] </span>];
+            } else {
+                newText += text[i];
+            }
+        }
+
+        return newText;
     }
 }
